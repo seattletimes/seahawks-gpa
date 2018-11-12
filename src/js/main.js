3f79d2c10afee3dd80d9d0f21ce7dff80a8079cc
ewebeck <ewebeck@seattletimes.com>
2018-11-11 17:06:23 -0800

    week 11

--- src/js/main.js
+++ src/js/main.js
@@ -7,7 +7,7 @@ require("chartist-plugin-tooltips");
 var Chartist = require("chartist");
 var Chart = require('chart.js');
 
-var gameStuff = window.gameData.slice(-8);
+var gameStuff = window.gameData.slice(-9);
 
 var labels = gameStuff.map(d => d.game);
 
